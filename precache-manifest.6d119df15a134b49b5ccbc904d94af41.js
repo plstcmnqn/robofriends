25c79c28e1b7a1bb8967e06c39765d001cba9a57
plstcmnqn <plstcmnqn@gmail.com>
2019-08-19 11:33:23 -0700

    Updates

---
+++ precache-manifest.6d119df15a134b49b5ccbc904d94af41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd9f2ef8eebd7310daff7dd0639521d",
+    "url": "/plstcmnqn/robofriends/index.html"
+  },
+  {
+    "revision": "262a95089cff2f611a64",
+    "url": "/plstcmnqn/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "24225cc33705fcf42de8",
+    "url": "/plstcmnqn/robofriends/static/css/main.c2f456cc.chunk.css"
+  },
+  {
+    "revision": "262a95089cff2f611a64",
+    "url": "/plstcmnqn/robofriends/static/js/2.7e9d9553.chunk.js"
+  },
+  {
+    "revision": "24225cc33705fcf42de8",
+    "url": "/plstcmnqn/robofriends/static/js/main.5ffadefb.chunk.js"
+  },
+  {
+    "revision": "f685020744065abd13bb",
+    "url": "/plstcmnqn/robofriends/static/js/runtime~main.9f236f23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/plstcmnqn/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
